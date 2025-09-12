eb883b02a5aed9d1d50e8898e92e9287d3a5b7fe
Ryan Abraham <ryan.ryanabraham@gmail.com>
2025-09-12 16:14:25 -0700

    spotless

--- src/main/java/frc/robot/subsystems/arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/arm/ArmConstants.java
@@ -104,9 +104,9 @@ public final class ArmConstants {
   public static final Angle[] reefRightScoringPosition = {
     Rotations.of(.32), Rotations.of(.56), Rotations.of(.67)
   };
-    public static final Angle[] reefLeftScoringPosition = {
-            Rotations.of(0.18), Rotations.of(0.18), Rotations.of(0.157)
-    };
+  public static final Angle[] reefLeftScoringPosition = {
+    Rotations.of(0.18), Rotations.of(0.18), Rotations.of(0.157)
+  };
 
   public static final class Sim {
     public static final double simGearing = 142.22;
