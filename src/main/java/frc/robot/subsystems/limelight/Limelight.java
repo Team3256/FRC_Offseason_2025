ce883885a96e813cc3fbeb0bd7587df8b838acca
isabella354 <isabellayu817@gmail.com>
2025-09-27 11:22:37 -0700

    Create Limelight.java

---
+++ src/main/java/frc/robot/subsystems/limelight/Limelight.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems.limelight;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import frc.robot.utils.LimelightHelpers;
+
+public class Limelight {
+
+    static final String sanitizeName(String name) {
+        if ("".equals(name) || name == null) {
+          return "limelight";
+        }
+        return name;
+      }
+    
+
+    public static NetworkTable getLimelightNTTable(String tableName) {
+        return NetworkTableInstance.getDefault().getTable(sanitizeName(tableName));
+    }
+
+    public static NetworkTableEntry getLimelightNTTableEntry(String tableName, String entryName) {
+        return getLimelightNTTable(tableName).getEntry(entryName);
+    }
+
+    public static double getLimelightNTDouble(String tableName, String entryName) {
+        return getLimelightNTTableEntry(tableName, entryName).getDouble(0.0);
+      }
+
+    
+    public static double getTX(String limelightName) {
+        return getLimelightNTDouble(limelightName, "tx");
+    }
+
+    public static double getTY(String limelightName) {
+        return getLimelightNTDouble(limelightName, "ty");
+    }
+
+    double tx = LimelightHelpers.getTX("limelight");  
+    double ty = LimelightHelpers.getTY("limelight");  
+    //boolean hasTarget = LimelightHelpers.getTV("limelight");  
+
+}
